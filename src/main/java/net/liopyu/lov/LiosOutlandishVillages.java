3fff38034be43ef7099aaa57d4a8f3d8f2265ea0
Liopyu <mistakensound@gmail.com>
2024-10-30 19:19:50 -0400

    Initial Commit

---
+++ src/main/java/net/liopyu/lov/LiosOutlandishVillages.java
@@ -0,0 +1,7 @@
+package net.liopyu.lov;
+import net.minecraftforge.fml.common.Mod;
+@Mod("lios_outlandish_villages")
+public class LiosOutlandishVillages {
+    public LiosOutlandishVillages() {
+    }
+}
